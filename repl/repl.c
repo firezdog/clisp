3ef9dc4ce835ecb7e0f3a4bd7a59171dfd0c2710
Alex Leibowitz <firezdog@yahoo.com>
2018-10-25 16:29:11 -0500

    added repl program

---
+++ repl/repl.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+//store input of up to 2048 characters in length
+static char input[2048];
+
+int main(int argc, char** argv) {
+    
+    //Info
+    puts("Lispy Version 1");
+    puts("Press Ctrl + c to exit");
+
+    while (1) {
+        // put to and get from the console (stdout or stdin)
+        fputs("lispy> ", stdout);
+        fgets(input, 2048, stdin);
+        printf("echo %s", input);
+    }
+
+    return 0;
+}
