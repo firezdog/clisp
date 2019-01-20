54d7de26dcb36b773960c0b748c083d03aff3eca
Alex Leibowitz <alexanderbleibowitz@gmail.com>
2019-01-20 12:49:28 -0600

    add algo to count leaves

--- ch7_evaluation/trees2.c
+++ ch7_evaluation/trees2.c
@@ -20,14 +20,6 @@ tree* createTree(int data) {
     return t;
 }
 
-int countBranches(tree* t) {
-    int branches = t->num_children;
-    for (int i = 0; i < t->num_children; i++) {
-        branches += countBranches(t->children[i]);
-    }
-    return branches;
-}
-
 void addBranch(tree* t, int data) {
     int children = t->num_children;
     tree **temp = t->children;
@@ -39,42 +31,30 @@ void addBranch(tree* t, int data) {
     t->num_children++;
 }
 
+int countBranches(tree* t) {
+    int branches = t->num_children;
+    for (int i = 0; i < t->num_children; i++) {
+        branches += countBranches(t->children[i]);
+    }
+    return branches;
+}
+
+int countLeaves(tree* t) {
+    if (t->num_children == 0) { return 1; }
+    int leaves = 0;
+    for (int i = 0; i < t->num_children; i++) {
+        leaves += countLeaves(t->children[i]);
+    }
+    return leaves;
+}
+
 int main() {
-    tree* t = createTree(5);
-    addBranch(t,10);
-    addBranch(t,10);
-    addBranch(t,10);
-    addBranch(t,10);
-    addBranch(t,10);
-    addBranch(t->children[0],10);
-    addBranch(t->children[0],10);
-    addBranch(t->children[0],10);
-    addBranch(t->children[0],10);
-
-    addBranch(t->children[1],10);
-    addBranch(t->children[1],10);
-    // for some reason, adding another branch crashes it?
-    // some kind of memory allocation problem -- see above for hack fix.
-    // must be because I need to allocate memory not just for number of children, but children of children also!
-    // but there's only an issue when I count the number of trees? -- how is that malloc?
-
-    // UPDATE (1/20 12:37pm) -- nope -- it was because I was (I hope) changing the value of the iterator! *rolls eyes*
-    addBranch(t->children[1],10);
-    addBranch(t->children[1],10);
-    addBranch(t->children[1],10);
-    addBranch(t->children[1],10);
-    addBranch(t->children[1],10);
-    addBranch(t->children[1],10);
-    addBranch(t->children[1],10);
-    addBranch(t->children[1],10);
-    addBranch(t->children[1],10);
-    addBranch(t->children[1],10);
-    addBranch(t->children[1],10);
-    addBranch(t->children[1],10);
-    addBranch(t->children[1],10);
-    addBranch(t->children[1],10);
-    addBranch(t->children[1],10);
-
-    printf("%i\n", countBranches(t));
+    tree* t = createTree(0);
+    addBranch(t, 0);
+    addBranch(t, 0);
+    addBranch(t, 0);
+    addBranch(t->children[0], 0);
+    addBranch(t->children[0], 0);
+    printf("%i\n", countLeaves(t));
     return 0;
 }
