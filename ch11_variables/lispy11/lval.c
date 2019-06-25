e3b539ac2b6772ac727962d1480a7da3ab170368
Alex Leibowitz <alexanderbleibowitz@gmail.com>
2019-06-24 21:49:09 -0500

    added lenv_put

--- ch11_variables/lispy11/lval.c
+++ ch11_variables/lispy11/lval.c
@@ -68,6 +68,26 @@ void lenv_del(lenv* v) {
     free(v);
 }
 
+void lenv_put(lenv* e, lval* a) {
+    /* puts a new value in the environment dictionary.  First, loop through and check if it is in the dictionary.  If so, delete the corresponding value and put the new one in.  If not, add it to the variables list, then add the corresponding assignment. 
+    
+    Note: here we put a copy in rather than the processed input itself -- presumably because the input is always getting deleted after the end of processing?
+    */
+    for (int i = 0; i < e->count; i++) {
+        if (!strcmp(e->assignments[i], a->op)) { 
+            lval_del(e->assignments[i]);
+            e->assignments[i] = lval_copy(a);
+            return; 
+        }
+    }
+    int n = ++e->count;
+    e->variables = realloc(e->variables, sizeof(char*) * n);
+    e->assignments = realloc(e->assignments, sizeof(lval*) * n);
+    e->variables[n - 1] = malloc(strlen(a->op) + 1);
+    strcpy(e->variables[n - 1], a->op);
+    e->assignments[n - 1] = lval_copy(a);
+}
+
 // copy an lval
 lval* lval_copy(lval* v) {
     lval* x = malloc(sizeof(lval));
