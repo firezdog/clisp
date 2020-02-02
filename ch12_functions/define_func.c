ee7f1c531d85097f82d2c6cd2f602ccbb499df6e
Alex Leibowitz <firezdog@gmail.com>
2020-02-01 18:44:09 -0600

    define_func: next step -- macro

--- ch12_functions/define_func.c
+++ ch12_functions/define_func.c
@@ -1,24 +1,30 @@
+#define LAMBDA(type_) ({\
+    type_ _; \
+})
+
 #include<stdio.h>
 
 // the problem
-void invoke_callback(int range, void (*callback_for_int) (int)) 
+void invoke_callback(int range, int (*callback_for_int) (int)) 
 {
     for (int i = 0; i < range; i++)
     {
-        callback_for_int(i);
+        printf("%d\n", callback_for_int(i));
     }
 }
 
 void main()
 {
-    invoke_callback(
-        10,
-        ({
-            void callback(int n) 
-            { 
-                printf("%d\n", n); 
-            } 
-            callback; 
-        })
-    );
+    // invoke_callback(
+    //     10,
+    //     ({
+    //         void callback(int n) 
+    //         { 
+    //             printf("%d\n", n); 
+    //         } 
+    //         callback; 
+    //     })
+    // );
+
+    invoke_callback(10, LAMBDA(int _(int n) { printf("%d\n", n); return n; }));
 }
