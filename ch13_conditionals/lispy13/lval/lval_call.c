b3e5032439875356525c5dc3b1ac1131c64410b3
Alex Leibowitz <firezdog@gmail.com>
2020-02-29 22:09:12 -0600

    refactored lval_call

--- ch13_conditionals/lispy13/lval/lval_call.c
+++ ch13_conditionals/lispy13/lval/lval_call.c
@@ -1,16 +1,27 @@
 #include "../lispy.h"
 
-lval* lval_call(lenv* e, lval* f, lval* a) {
-    /* Given a function, an lval with assignments, and an environment, call the function with those assignments in that environment.*/
-    if (f->builtin) { return f->builtin(e, a); }
-    int* args_required = &(f->formals->cell_count);
-    int* args_supplied = &(a->cell_count);
-    while (*args_supplied) {
-        lval* dummy_param = lval_qexpr();
-        dummy_param->op = "NULL";
-        lval* param = *args_required == 0 ? 
-            dummy_param :
-            lval_pop(f->formals, 0);
+lval* get_next_param(int args_required, lval* f) 
+{
+    // handle 0 param case and get next param
+    lval* dummy_param = lval_qexpr();
+    dummy_param->op = "NULL";
+    return args_required == 0 ?
+        dummy_param :
+        lval_pop(f->formals, 0);
+}
+
+lval* eval_params(
+    int* args_required, 
+    int* args_remaining, 
+    lenv* e, 
+    lval* f, 
+    lval* a
+)
+{
+    while (*args_remaining) {
+        lval* param = get_next_param(*args_required, f);
+
+        // next param is &: pop & and param list, then fill the function
         if (!strcmp(param->op, "&")) {
             LASSERT(
                 a, 
@@ -22,27 +33,29 @@ lval* lval_call(lenv* e, lval* f, lval* a) {
             lval_del(arg_list_param); 
             break;
         } else {
-            if (*args_required + 1 != a->cell_count) 
-                return lval_err(
-                    "Too many arguments passed to anonymous function. Expected %i, got %i.",
-                    *args_required + 1,
-                    *args_supplied
-                );
+        // otherwise pop next arg and fill param in function
             lval* arg = lval_pop(a, 0);
             lenv_put(f->env, param, arg);
             lval_del(arg);
         }
+
         lval_del(param);
     }
+}
+
+lval* lval_call(lenv* e, lval* f, lval* a) {
+    /* Given a function, an lval with assignments, and an environment, call the function with those assignments in that environment.*/
+    if (f->builtin) { return f->builtin(e, a); }
+    int* args_required = &(f->formals->cell_count);
+    int* args_supplied = &(a->cell_count);
+
+    lval* error;
+    
+    error = eval_params(args_required, args_supplied, e, f, a);
+    if (error != NULL) return error; 
     lval_del(a);
-    if (*args_required > 0 && !strcmp(f->formals->cell[0]->op, "&")) {
-        lval_del(lval_pop(f->formals, 0));
-        LASSERT(NULL, f->formals->cell_count == 1,  "Function format invalid: '&' symbol must be followed by one argument naming the list of variable arguments");
-        lval* param = lval_pop(f->formals, 0);
-        lval* arg = lval_qexpr();
-        lenv_put(f->env, param, arg);
-        lval_del(param); lval_del(arg);
-    }
+
+    // return evaluation
     f->env->parent_environment = e;
     if (*args_required == 0) {
         return builtin_eval(
@@ -53,5 +66,7 @@ lval* lval_call(lenv* e, lval* f, lval* a) {
             )
         );
     }
+
+    // return partial evaluation
     return lval_copy(f);
 }
